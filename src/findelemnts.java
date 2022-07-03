04a62b7f0215653bfa2f27ced4c663cf97864d3f
Sudha <gangulasudha@gmail.com>
2022-07-03 10:23:33 -0400

    This is First commit....

---
+++ src/findelemnts.java
@@ -0,0 +1,21 @@
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class findelemnts {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("https://demo.nopcommerce.com/register");
+		
+		
+
+	}
+
+}
